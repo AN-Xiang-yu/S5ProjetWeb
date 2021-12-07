7103bb7b706d3d1feb45347bbe4bbefb09be497e
AN <409227823@qq.com>
2021-12-07 13:09:08 +0100

    Templette

--- js/commun.js
+++ js/commun.js
@@ -1,3 +1,6 @@
+var tailleEcranOrdinateur = 1200;
+var tailleEcranSmartphone = 768;
+
 class MonHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -6,29 +9,29 @@ class MonHeader extends HTMLElement {
                 <ul class="flex align_center justify_content_center">
                     <li class="flex_15">
                         <div class="flex justify_content_center">
-                            <a href="accueil.html">
+                            <a class="ajouter_chemin" href="accueil.html">
                                 <!-- logo sur ordinateur -->
-                                <img class="hight100" src="../img/logo-phone.png" alt="logo d'ordinateur">
+                                <img class="hight100 ajouter_chemin_image" src="../img/logo-phone.png" alt="logo d'ordinateur">
                                 <!-- logo sur tablette/phone -->
                                 <!-- <img src="../img/logo.png" alt=""> -->
                             </a>
                         </div>
                     </li>
-                    <li class="flex_10">
-                        <a href="accueil.html">Accueil</a></li>
-                    <li class="flex_10">
-                        <a href="contact.html">Contact</a></li>
-                    <li class="flex_10">Activité</li>
-                    <li class="flex_10">Collabs</li>
-                    <li class="flex_10">La méditation</li>
-                    <li class="flex_10">
+                    <li id="accueil" class="flex_10">
+                        <a class="ajouter_chemin" href="accueil.html">Accueil</a></li>
+                    <li id="contact" class="flex_10">
+                        <a class="ajouter_chemin" href="contact.html">Contact</a></li>
+                    <li id="activite" class="flex_10">Activité</li>
+                    <li id="collabs" class="flex_10">Collabs</li>
+                    <li id="meditation" class="flex_10">La méditation</li>
+                    <li id="produit" class="flex_10">
                         <a href="../page/produit/produit.html">Produit</a>
                     </li>
-                    <li class="flex_10">
-                        <a href="a_propos.html">À propos</a>
+                    <li id="a_propos" class="flex_10">
+                        <a class="ajouter_chemin" href="a_propos.html">À propos</a>
                     </li>
-                    <li class="flex_10">
-                        <a href="nous_rejoindre.html">Nous rejoindre</a>
+                    <li id="nous_rejoindre" class="flex_10">
+                        <a class="ajouter_chemin" href="nous_rejoindre.html">Nous rejoindre</a>
                     </li>
                 </ul>
             </nav>
@@ -51,6 +54,49 @@ class MonFooter extends HTMLElement {
 }
 
 customElements.define('mon-footer', MonFooter)
+
+//recupere le nom de page actuel
+function recupererNomPage() {
+    var locationHrefComplete = location.href.split('/')
+    var nomFichier = locationHrefComplete[locationHrefComplete.length - 1]
+    var idNav = nomFichier.split('.')[0]
+    return idNav
+}
+
+//ajouter css dans menu de navigation pour la page actuelle
+function ajouterCssMenuNavigation() {
+    var liChange = document.getElementById(recupererNomPage())
+    liChange.classList.add("page_actuel")
+}
+
+//changer le chemin si les pages sont pas dans les répertoires
+function ajouterChemin() {
+    var nomPage = recupererNomPage()
+    var memeAChange = document.getElementById(nomPage)
+
+    //le lien dans le menu de navigation de la page courante va naviguer sur elle même
+    memeAChange.children[0].href = "#"
+
+    switch (nomPage) {
+        case "accueil", "contact", "a_propos", "nous_rejoindre":
+            break;
+        default:
+            var aChange = document.getElementsByClassName("ajouter_chemin")
+            var imgChange = document.getElementsByClassName("ajouter_chemin_image")
+
+            //modifier les liens dans menu de navigation
+            for (let a of aChange) {
+                a.href = "../" + a.getAttribute("href")
+            }
+            //modifier les sources des images dans menu de navigation
+            for (let img of imgChange) {
+                img.setAttribute("src", "../" + img.getAttribute("src"))
+            }
+            break;
+    }
+}
+
+
 
 function changerNav() {
     let largueurEcran = $(window).width();
